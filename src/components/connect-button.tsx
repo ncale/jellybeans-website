9a690e3f20c20946eed8ea45201ebd7e3bf3d6f2
Nick Brodeur <106794061+ncale@users.noreply.github.com>
2024-09-16 14:02:59 -0400

    chore: build custom connect button

---
+++ src/components/connect-button.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { Button } from "./ui/button";
+
+export default function CustomConnectButton() {
+  return (
+    <ConnectButton.Custom>
+      {({
+        account,
+        chain,
+        openAccountModal,
+        openChainModal,
+        openConnectModal,
+        authenticationStatus,
+        mounted,
+      }) => {
+        const connected =
+          mounted &&
+          account &&
+          chain &&
+          (!authenticationStatus || authenticationStatus === "authenticated");
+        return (
+          <div
+            {...(!mounted && {
+              "aria-hidden": true,
+              className: "opacity-0 pointer-events-none select-none",
+            })}
+          >
+            {(() => {
+              if (!connected) {
+                return (
+                  <Button onClick={openConnectModal} type="button">
+                    Connect
+                  </Button>
+                );
+              }
+              if (chain.unsupported) {
+                return (
+                  <button onClick={openChainModal} type="button">
+                    Wrong network
+                  </button>
+                );
+              }
+              return (
+                <div className="flex gap-3">
+                  <Button onClick={openAccountModal} type="button">
+                    {account.displayName}
+                    {account.displayBalance
+                      ? ` (${account.displayBalance})`
+                      : ""}
+                  </Button>
+                </div>
+              );
+            })()}
+          </div>
+        );
+      }}
+    </ConnectButton.Custom>
+  );
+}
