a5b54f967d329005755cb197f38b31e4dc1752f9
Nick Brodeur <106794061+ncale@users.noreply.github.com>
2024-09-17 13:36:32 -0400

    chore: scaffold recent guesses table

--- src/app/round/[num]/_pages/current-round.tsx
+++ src/app/round/[num]/_pages/current-round.tsx
@@ -4,11 +4,14 @@ import { CurrentRoundData } from "@/lib/types";
 import Details from "../_components/details";
 import Pagination from "../_components/pagination";
 import Submit from "../_components/submit";
+import LeaderboardTable from "../_components/leaderboard-table";
 
 export default function CurrentRoundPage({ data }: { data: CurrentRoundData }) {
   return (
     <>
-      <section className="w-1/2 bg-green-50 p-2">picture</section>
+      <section className="w-1/2 p-2">
+        <div className="h-full w-full bg-red-50" />
+      </section>
       <section className="w-1/2 space-y-8 p-2">
         <Pagination currentPage={data.round} />
         <Details
@@ -17,7 +20,7 @@ export default function CurrentRoundPage({ data }: { data: CurrentRoundData }) {
           submissionDeadline={data.submissionDeadline}
         />
         <Submit round={data.round} feeAmount={data.feeAmount} />
-        {/* <LeaderboardTable /> */}
+        <LeaderboardTable round={data.round} />
       </section>
     </>
   );
