91936861b7039141ae6f0e55bf1c0f18e0721693
Nick Brodeur <106794061+ncale@users.noreply.github.com>
2024-09-18 15:30:10 -0400

    chore: update types to be used with api rather than contract reads

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,4 @@
+import { Hex } from "viem";
 import { z } from "zod";
 
 // URL PARAMS
@@ -7,34 +8,50 @@ export type NumType = z.infer<typeof numSchema>;
 
 // DATA
 
-export type RawRoundData = readonly [string, bigint, bigint, bigint, bigint, boolean];
+export type RawRoundData = {
+  round: {
+    id: string; // -> bigint
+    question: string;
+    submissionDeadline: string; // -> bigint
+    potAmount: string; // -> bigint
+    feeAmount: string; // -> bigint
+    initRoundTxnHash: Hex;
+    isFinalized: boolean;
+    correctAnswer: string; // -> bigint
+    winningAnswer: string; // -> bigint
+    winners: Hex[];
+    setCorrectAnswerTxnHash: Hex;
+  };
+};
 
-export type PreviousRoundData = {
-  round: number;
+export type RoundData = ActiveRoundData | PastRoundData;
+
+export type ActiveRoundData = {
+  roundState: "active";
+  id: number;
   question: string;
   submissionDeadline: bigint;
   potAmount: bigint;
   feeAmount: bigint;
-  correctAnswer: bigint;
+  initRoundTxnHash: Hex;
+  isFinalized: false;
+  correctAnswer: null;
+  winningAnswer: null;
+  winners: null;
+  setCorrectAnswerTxnHash: null;
+};
+
+export type PastRoundData = {
+  roundState: "past";
+  id: number;
+  question: string;
+  submissionDeadline: bigint;
+  potAmount: bigint;
+  feeAmount: bigint;
+  initRoundTxnHash: Hex;
   isFinalized: true;
-};
-
-export type CurrentRoundData = {
-  round: number;
-  question: string;
-  submissionDeadline: bigint;
-  potAmount: bigint;
-  feeAmount: bigint;
-  correctAnswer: 0n;
-  isFinalized: false;
-};
-
-export type FutureRoundData = {
-  round: number;
-  question: "";
-  submissionDeadline: 0n;
-  potAmount: 0n;
-  feeAmount: 0n;
-  correctAnswer: 0n;
-  isFinalized: false;
+  correctAnswer: bigint;
+  winningAnswer: bigint;
+  winners: Hex[];
+  setCorrectAnswerTxnHash: Hex;
 };
