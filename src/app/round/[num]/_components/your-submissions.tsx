ad6f437b7eda7726945593a9ebe64b57e51b4c54
Nick Brodeur <106794061+ncale@users.noreply.github.com>
2024-09-19 18:13:07 -0400

    fix: fix render order on `YourSubmissions` component to avoid loading / n/a / other ui conflicts

--- src/app/round/[num]/_components/your-submissions.tsx
+++ src/app/round/[num]/_components/your-submissions.tsx
@@ -7,6 +7,17 @@ import { useQuery } from "@tanstack/react-query";
 import { useAccount } from "wagmi";
 
 export default function YourSubmissions({ round }: { round: number }) {
+  return (
+    <div className="mt-4 space-y-1">
+      <h3 className="font-medium">Your Submissions</h3>
+      <div className="flex flex-wrap gap-1">
+        <MappedPills round={round} />
+      </div>
+    </div>
+  );
+}
+
+function MappedPills({ round }: { round: number }) {
   const { address } = useAccount();
 
   const { data, isLoading, isSuccess } = useQuery({
@@ -15,20 +26,12 @@ export default function YourSubmissions({ round }: { round: number }) {
     enabled: !!address,
   });
 
-  return (
-    <div className="mt-4 space-y-1">
-      <h3 className="font-medium">Your Submissions</h3>
-      <div className="flex flex-wrap gap-1">
-        {isLoading && <Pill variant="outline">log in</Pill>}
-        {isLoading && <>Loading...</>}
-        {(!isSuccess || data.submissions.items.length === 0) && <Pill variant="outline">n/a</Pill>}
-        {isSuccess &&
-          data.submissions.items.map((sub) => (
-            <a href={`${BLOCKSCOUT_BASE_URL}tx/${sub.txnHash}/`} target="_blank" key={sub.txnHash}>
-              <Pill variant="outline">{sub.entry}</Pill>
-            </a>
-          ))}
-      </div>
-    </div>
-  );
+  if (isLoading) return <Pill variant="outline">Loading...</Pill>;
+  if (!isSuccess) return <Pill variant="outline">n/a</Pill>;
+
+  return data.submissions.items.map((sub) => (
+    <a href={`${BLOCKSCOUT_BASE_URL}tx/${sub.txnHash}/`} target="_blank" key={sub.txnHash}>
+      <Pill variant="outline">{sub.entry}</Pill>
+    </a>
+  ));
 }
