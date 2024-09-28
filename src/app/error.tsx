5b876d40c1beea7f466397de58761a9ca18a6aab
Nick Brodeur <106794061+ncale@users.noreply.github.com>
2024-09-28 15:24:20 -0500

    chore: add `error` page and update link in the `not-found` page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,10 +1,12 @@
+"use client";
+
 import Link from "next/link";
 
 export default function NotFound() {
   return (
     <>
       <section>
-        <h1 className="text-2xl font-bold">{"Uh oh! We can't find this page"}</h1>
+        <h1 className="text-2xl font-bold">{"Oops! Something went wrong."}</h1>
 
         <Link href="/" className="hover:underline">
           Go home
